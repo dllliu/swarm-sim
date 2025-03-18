958cabdbc8e5bb3c17e802cbb6c7b3e40a29d76a
dliu <dliu30@stuy.edu>
2025-03-18 12:07:59 -0400

    manage beacon state responsibility in beacon not boundary point, physics every 5 frames

--- boundary_manager.py
+++ boundary_manager.py
@@ -1,17 +1,15 @@
 import numpy as np
 
-class BoundaryManager:
-    def __init__(self, center_x, center_y, speed, direction, minX, maxX, minY, maxY, thres_dist):
+class BoundaryPoint:
+    def __init__(self, center_x, center_y, speed, direction, minX, maxX, minY, maxY):
         self.center_x = center_x
         self.center_y = center_y
         self.speed = speed
         self.direction = direction
-        self.thres_dist = thres_dist
         self.minX = minX
         self.maxX = maxX
         self.minY = minY
         self.maxY = maxY
-        self.thres_dist = thres_dist
 
         self.V_bx = speed * np.cos(direction)
         self.V_by = speed * np.sin(direction)
@@ -24,18 +22,3 @@ class BoundaryManager:
             self.V_bx = -self.V_bx
         if self.center_y < self.minY or self.center_y > self.maxY:
             self.V_by = -self.V_by
-
-    def is_inside_boundary(self, x, y):
-        dist = np.sqrt((x- self.center_x)**2 + (y-self.center_y)**2)
-        if dist < self.thres_dist:
-            return True
-        return False
-
-    def update_beacons(self, beacons, new_beacon_j):
-        for beacon in beacons:
-            if self.is_inside_boundary(beacon.x, beacon.y):
-                beacon.set_beacon_J(new_beacon_j)
-                beacon.active = True
-            else:
-                beacon.set_beacon_J(0)
-                beacon.active = False
